e4b722e7bf0522d12097f5a8028f0f1e3eacc406
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-06-09 15:10:04 -0500

    add home

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../UserContext";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const { user, setUser } = useContext(UserContext);
+  const setAsJhon = () => {
+    const john = {
+      name: "Jhon",
+      email: "jhon@hotmail.com",
+      password: "123",
+      id: "123",
+    };
+    setUser(john);
+  };
+  const setAsTom = () => {
+    const tom = {
+      name: "Tom",
+      email: "tom@hotmail.com",
+      password: "456",
+      id: "456",
+    };
+    setUser(tom);
+  };
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home {JSON.stringify(user)}</h1>
+      <button onClick={setAsJhon}>Jhon</button>
+      <button onClick={setAsTom}>Tom</button>
+      <Link to={"/chat"}>
+        <button>Go to chat</button>
+      </Link>
     </div>
   );
 };
