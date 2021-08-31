d1fd0e5e24eed5b8ecd8ca8be87b3838f837c00b
George2013 <jorge.hernandez@qrvey.com>
2021-08-31 10:14:11 -0500

    Join

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,13 +1,23 @@
-import React,{ useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../UserContext";
-import { Link,useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import io, { Socket } from "socket.io-client";
+import { ENDPT } from "../../services/services";
+let socket;
 
 const Chat = () => {
+  let { room_id, room_name } = useParams();
+  useEffect(() => {
+    socket = io(ENDPT, { transports: ["websocket"] });
+    socket.emit("join", { name: user.name, room_id, user_id: user.id });
+  }, []);
   const { user, setUser } = useContext(UserContext);
-  let {room_id,room_name} = useParams();
+
   return (
     <div>
-      <div>{room_id} {room_name}</div>
+      <div>
+        {room_id} {room_name}
+      </div>
       <h1>Chat {JSON.stringify(user)}</h1>
       <Link to={"/"}>
         <button>Go to home a</button>
