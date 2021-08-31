2543949a0c976f9a487c64ce13cd5c4144ec7316
George2013 <jorge.hernandez@qrvey.com>
2021-08-31 10:48:18 -0500

    update chat view

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../UserContext";
 import { Link, useParams } from "react-router-dom";
 import io, { Socket } from "socket.io-client";
@@ -6,22 +6,39 @@ import { ENDPT } from "../../services/services";
 let socket;
 
 const Chat = () => {
+  const { user, setUser } = useContext(UserContext);
   let { room_id, room_name } = useParams();
+  const [message, setMessage] = useState("");
   useEffect(() => {
     socket = io(ENDPT, { transports: ["websocket"] });
     socket.emit("join", { name: user.name, room_id, user_id: user.id });
   }, []);
-  const { user, setUser } = useContext(UserContext);
-
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      console.log(message);
+      socket.emit("set_message", message, room_id, () => setMessage(""));
+    }
+  };
   return (
     <div>
       <div>
         {room_id} {room_name}
       </div>
       <h1>Chat {JSON.stringify(user)}</h1>
-      <Link to={"/"}>
-        <button>Go to home a</button>
-      </Link>
+      <form action="" onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={message}
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        ></input>
+        <button>Send message</button>
+      </form>
     </div>
   );
 };
